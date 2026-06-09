84736d673e39e8c425e993a8f7c6a73a4be88ec9
Corina <corinacr01@gmail.com>
2026-06-09 17:01:04 -0400

    App con waitress y correo listo

--- app.py
+++ app.py
@@ -30,7 +30,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-
+app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', '')
+app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', '')
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
