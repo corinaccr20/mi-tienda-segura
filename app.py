4f2661a8b9edba415d6914161262bb8a209c7b3c
Corina <corinacr01@gmail.com>
2026-06-09 17:24:03 -0400

    Fix: Flask escucha en 0.0.0.0 para Render

--- app.py
+++ app.py
@@ -167,4 +167,5 @@ with app.app_context():
         db.session.commit()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)), debug=False)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
